62312073eea48059564a3d55b1d66ca84ed56dae
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2019-03-11 18:19:13 -0400

    Added hal configuration files for moffitt_merfish1

---
+++ storm_control/hal4000/batFiles/moffitt_merfish1.bat
@@ -0,0 +1,2 @@
+rem Activate virtual environment and start HAL.
+cmd /k C:\Users\MERFISH1\AppData\Local\conda\conda\envs\myenv\python C:\Users\MERFISH1\Code\storm-control\storm_control\hal4000\hal4000.py C:\Users\MERFISH1\Code\storm-control\storm_control\hal4000\xml\moffitt_merfish1_config.xml
