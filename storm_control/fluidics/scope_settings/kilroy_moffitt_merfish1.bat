eea7d199fd86ce2a0e08469945b26bce304d45c5
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2019-03-11 17:47:19 -0400

    Updated kilroy configuration on Moffitt MERFISH1 and added bat file

---
+++ storm_control/fluidics/scope_settings/kilroy_moffitt_merfish1.bat
@@ -0,0 +1,1 @@
+cmd /k C:\Users\MERFISH1\AppData\Local\conda\conda\envs\myenv\python C:\Users\MERFISH1\Code\storm-control\storm_control\fluidics\kilroy.py C:\Users\MERFISH1\Code\storm-control\storm_control\fluidics\scope_settings\kilroy_settings_moffitt_merfish1.xml
