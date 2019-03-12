30779e95a98e32c5a567f7df911bbe9de1f085c9
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2019-03-12 08:53:48 -0400

    Added bat file for hal and updated the launch style for the bat files for kilroy and dave

--- storm_control/hal4000/batFiles/moffitt_merfish1.bat
+++ storm_control/hal4000/batFiles/moffitt_merfish1.bat
@@ -1,2 +1,4 @@
-rem Activate virtual environment and start HAL.
-cmd /k C:\Users\MERFISH1\AppData\Local\conda\conda\envs\myenv\python C:\Users\MERFISH1\Code\storm-control\storm_control\hal4000\hal4000.py C:\Users\MERFISH1\Code\storm-control\storm_control\hal4000\xml\moffitt_merfish1_config.xml
+set sc_base=C:\Users\MERFISH1\Code\storm-control\
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+call activate myenv
+cmd /k python %sc_base%\storm_control\hal4000\hal4000.py %sc_base%\storm_control\hal4000\xml\moffitt_merfish1_config.xml
