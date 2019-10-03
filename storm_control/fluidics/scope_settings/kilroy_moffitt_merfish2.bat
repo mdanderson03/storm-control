15ce2dc96da1f6eb381d965305985070e8f76a17
Jeffrey Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2019-10-03 07:35:56 -0400

    Added kilroy configuration for Moffitt Lab MERFISH2

---
+++ storm_control/fluidics/scope_settings/kilroy_moffitt_merfish2.bat
@@ -0,0 +1,4 @@
+set sc_base=C:\Users\MERFISH2\Code\storm-control\
+call C:\Users\MERFISH2\Anaconda3\Scripts\activate.bat
+call activate merfish2_env
+cmd /k python %sc_base%\storm_control\fluidics\kilroy.py %sc_base%\storm_control\fluidics\scope_settings\kilroy_settings_moffitt_merfish2.xml
