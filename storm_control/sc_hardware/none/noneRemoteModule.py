feaac2c952e51c5561bedc97ecc07ae1150187d8
Hazen Babcock <hbabcock@mac.com>
2020-03-27 16:48:28 -0400

    Change remote server to a QApplication so that we can use QThreads, etc..

--- storm_control/sc_hardware/none/noneRemoteModule.py
+++ storm_control/sc_hardware/none/noneRemoteModule.py
@@ -4,22 +4,30 @@ A remote HAL module emulator.
 
 Hazen 03/20
 """
+import signal
+import sys
+
+from PyQt5 import QtCore, QtWidgets
 
 import storm_control.sc_hardware.baseClasses.remoteHardware as remoteHardware
 
 
 class NoneHardwareServerModule(remoteHardware.RemoteHardwareServerModule):
-    pass
+
+    def processMessage(self, r_message):
+        super().processMessage(r_message)
+        print(r_message.getSourceName(), r_message.m_type)
 
 
 if (__name__ == "__main__"):
-    nhsm = NoneHardwareServerModule(ip_address = "tcp://*:5556")
-    nhsm.bind()
+
+    app = QtWidgets.QApplication(sys.argv)
+
+    nhsm = NoneHardwareServerModule()
+    rhs = remoteHardware.RemoteHardwareServer(ip_address = "tcp://*:5556",
+                                              module = nhsm)
+
+    # Need this so that CTRL-C works.
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     
-    while True:
-        print("waiting for next message")
-        message = nhsm.nextMessage()
-        print("responding")
-        nhsm.sendWait(False)
-        print(message)
-        nhsm.sendResponse(message)
+    app.exec_()
