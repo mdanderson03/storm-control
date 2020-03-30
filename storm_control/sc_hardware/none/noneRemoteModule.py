9294b67307456c4ce537aa69d7ac131d5c898909
Hazen Babcock <hbabcock@mac.com>
2020-03-30 15:36:03 -0400

    Switch to using timers. Get bi-directional communication working.

--- storm_control/sc_hardware/none/noneRemoteModule.py
+++ storm_control/sc_hardware/none/noneRemoteModule.py
@@ -16,24 +16,41 @@ import storm_control.sc_hardware.baseClasses.remoteHardware as remoteHardware
 import storm_control.hal4000.halLib.halMessage as halMessage
 
 
+class NoneRemoteHardwareModule(remoteHardware.RemoteHardwareModule):
+    def remoteMessage(self, r_message):
+        print("Received", r_message)
+
+
 class NoneHardwareServerModule(remoteHardware.RemoteHardwareServerModule):
     """
     A simple remote parameter manipulation module.
     """
     def __init__(self, **kwds):
         super().__init__(**kwds)
-        
-        self.module_name = "remote"
-        
-        self.parameters = params.StormXMLObject()
 
+        self.module_name = "remote"
+
+        self.message_count = 0
+        self.message_timer = QtCore.QTimer(self)
+        self.message_timer.setInterval(1000)
+        self.message_timer.timeout.connect(self.handleMessageTimer)
+         
+        self.parameters = params.StormXMLObject()
         self.parameters.add(params.ParameterSetString(description = "Mode",
                                                       name = "mode",
                                                       value = "mode1",
                                                       allowed = ["mode1", "mode2", "mode3"]))
 
+    def cleanUp(self):
+        super().cleanUp()
+        self.message_timer.stop()
+        
+    def handleMessageTimer(self):
+        self.message_count += 1
+        self.sendMessage.emit("message {0:d}".format(self.message_count))
+        
     def processMessage(self, r_message):
-        print(r_message.getSourceName(), r_message.m_type)
+        print(r_message.getSourceName(), "'" + r_message.m_type + "'")
 
         if r_message.isType("current parameters"):
             r_message.addResponse(halMessage.HalMessageResponse(source = self.module_name,
@@ -47,8 +64,11 @@ class NoneHardwareServerModule(remoteHardware.RemoteHardwareServerModule):
             r_message.addResponse(halMessage.HalMessageResponse(source = self.module_name,
                                                                 data = {"new parameters" : self.parameters}))
             print("New mode is:", self.parameters.get("mode"))
+            
+        elif r_message.isType("start"):
+            self.message_count = 0
+            self.message_timer.start()
 
-        self.sendWait.emit(False)
         self.sendResponse.emit(r_message)
 
 
@@ -57,7 +77,8 @@ if (__name__ == "__main__"):
     app = QtWidgets.QApplication(sys.argv)
 
     nhsm = NoneHardwareServerModule()
-    rhs = remoteHardware.RemoteHardwareServer(ip_address = "tcp://*:5556",
+    rhs = remoteHardware.RemoteHardwareServer(ip_address_hal = "tcp://localhost:5557",
+                                              ip_address_remote = "tcp://*:5556",
                                               module = nhsm)
 
     # Need this so that CTRL-C works.
