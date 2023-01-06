40962a0c816f7f8f6669b26b1b9fa9dd456832b3
Jeff Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2023-01-06 16:32:39 -0500

    Added configuraton intermediate for M6

--- storm_control/hal4000/batFiles/moffitt_merfish6.bat
+++ storm_control/hal4000/batFiles/moffitt_merfish6.bat
@@ -1,4 +1,4 @@
 set sc_base=C:\Users\MERFISH6\Code\storm-control\
-call C:\Users\MERFISH6\anaconda3\Scripts\activate.bat
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
 call activate hal_env
 cmd /k python %sc_base%\storm_control\hal4000\hal4000.py %sc_base%\storm_control\hal4000\xml\moffitt_merfish6_config.xml
--- storm_control/steve/steve_merfish5.bat
+++
@@ -1,4 +0,0 @@
-set sc_base=C:\Users\MERFISH5\Code\storm-control\
-call C:\Users\MERFISH5\anaconda3\Scripts\activate.bat
-call activate hal_env
-cmd /k python %sc_base%\storm_control\steve\steve.py 
