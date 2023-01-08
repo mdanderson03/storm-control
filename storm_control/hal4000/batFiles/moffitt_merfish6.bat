202feebb4e68cfd4941f6dc3424a64a84d7f3062
Jeff Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2023-01-08 13:59:52 -0500

    Updating M6 bat and config files

--- storm_control/hal4000/batFiles/moffitt_merfish6.bat
+++ storm_control/hal4000/batFiles/moffitt_merfish6.bat
@@ -1,4 +1,3 @@
 set sc_base=C:\Users\MERFISH6\Code\storm-control\
-call C:\ProgramData\Anaconda3\Scripts\activate.bat
-call activate hal_env
+call C:\Users\MERFISH6\envs\merfish6_env\Scripts\activate.bat
 cmd /k python %sc_base%\storm_control\hal4000\hal4000.py %sc_base%\storm_control\hal4000\xml\moffitt_merfish6_config.xml
--- storm_control/steve/steve_merfish6.bat
+++ storm_control/steve/steve_merfish6.bat
@@ -1,4 +1,4 @@
 set sc_base=C:\Users\MERFISH6\Code\storm-control\
 call C:\ProgramData\Anaconda3\Scripts\activate.bat
-call activate hal_env
+call activate merfish6_env
 cmd /k python %sc_base%\storm_control\steve\steve.py 
