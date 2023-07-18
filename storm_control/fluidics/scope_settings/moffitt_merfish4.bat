0fec83188a678d71eb59c32979d2ac4ef838ac71
Jeff Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2023-07-18 15:29:31 -0400

    Added the M4a first configuration files

---
+++ storm_control/fluidics/scope_settings/moffitt_merfish4.bat
@@ -0,0 +1,4 @@
+set sc_base=C:\Users\MERFISH5\Code\storm-control\
+call C:\Users\MERFISH5\anaconda3\Scripts\activate.bat
+call activate hal_env
+cmd /k python %sc_base%\storm_control\fluidics\kilroy.py %sc_base%\storm_control\fluidics\scope_settings\kilroy_settings_merfish4.xml
---
+++ storm_control/hal4000/batFiles/moffitt_merfish4sub.bat
@@ -0,0 +1,4 @@
+set sc_base=C:\Users\MERFISH5\Code\storm-control\
+call C:\Users\MERFISH5\anaconda3\Scripts\activate.bat
+call activate hal_env
+cmd /k python %sc_base%\storm_control\hal4000\hal4000.py %sc_base%\storm_control\hal4000\xml\moffitt_merfish4sub2_config.xml
