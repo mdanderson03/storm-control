d9e52271364f1293e8be8fd8dd79645731196d31
Jeff Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2023-07-20 15:47:51 -0400

    Updating some configuration files on M4_dev

--- storm_control/steve/steve_merfish4.bat
+++ storm_control/steve/steve_merfish4.bat
@@ -1,4 +1,4 @@
 set sc_base=C:\Users\MERFISH5\Code\storm-control\
 call C:\Users\MERFISH5\anaconda3\Scripts\activate.bat
 call activate merfish4_env
-cmd /k python %sc_base%\storm_control\hal4000\hal4000.py %sc_base%\storm_control\hal4000\xml\moffitt_merfish4sub2_config.xml
+cmd /k python %sc_base%\storm_control\steve\steve.py  
