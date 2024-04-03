18dccd567f5c03d338a35e4f096a8b09ec6feed9
Jeff Moffitt <jeffrey.moffitt@childrens.harvard.edu>
2024-04-03 10:49:56 -0400

    Updated M6_dev with configuration modifications

---
+++ storm_control/fluidics/scope_settings/kilroy_moffitt_merfish4_test.bat
@@ -0,0 +1,3 @@
+set sc_base=C:\Users\MERFISH6\Code\storm-control\
+call C:\Users\MERFISH6\envs\merfish6_env\Scripts\activate.bat
+cmd /k python %sc_base%\storm_control\fluidics\kilroy.py %sc_base%\storm_control\fluidics\scope_settings\kilroy_settings_moffitt_merfish4_test.xml
